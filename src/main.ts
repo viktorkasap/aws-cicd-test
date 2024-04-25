920bb7d0b4581e006c31115932e114534b30ba11
kasapvictor <kasap.victor@gmail.com>
2024-04-24 18:08:23 -0700

    feat: Added Invalidation CloudFront

--- src/main.ts
+++ src/main.ts
@@ -11,7 +11,7 @@ document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
     <a href="https://www.typescriptlang.org/" target="_blank">
       <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
     </a>
-    <h1>Vite + TypeScript</h1>
+    <h1>AWS S3 + GitHub CI/CD</h1>
     <div class="card">
       <button id="counter" type="button"></button>
     </div>
